0d7b8004604f53eb762193a701f2db09f6a6172d
bryanjbassett <bassett.91@gmail.com>
2016-04-10 09:55:35 -0400

    Update BowlingGame.java

--- BowlingGame/src/BowlingGame.java
+++ BowlingGame/src/BowlingGame.java
@@ -1,6 +1,16 @@
-/**
- *
- * @author Bryan James Bassett
+/**----------------------------------------------------------------------------------*
+ *______________   _____   _   _  ______  ___   _____ _____ _____ _____ _____ _     
+ *| ___ \ ___ \ \ / / _ \ | \ | | | ___ \/ _ \ /  ___/  ___|  ___|_   _|_   _( )    
+ *| |_/ / |_/ /\ V / /_\ \|  \| | | |_/ / /_\ \\ `--.\ `--.| |__   | |   | | |/ ___ 
+ *| ___ \    /  \ /|  _  || . ` | | ___ \  _  | `--. \`--. \  __|  | |   | |   / __|
+ *| |_/ / |\ \  | || | | || |\  | | |_/ / | | |/\__/ /\__/ / |___  | |   | |   \__ \
+ *\____/\_| \_| \_/\_| |_/\_| \_/ \____/\_| |_/\____/\____/\____/  \_/   \_/   |___/
+ *.----.  .----. .-. . .-..-.   .-..-. .-. .---.    .-. .-.  .--.  .---.  .--.  
+ *| {}  }/  {}  \| |/ \| || |   | ||  `| |/   __}   | |/ /  / {} \{_   _}/ {} \ 
+ *| {}  }\      /|  .'.  || `--.| || |\  |\  {_ }   | |\ \ /  /\  \ | | /  /\  \
+ *`----'  `----' `-'   `-'`----'`-'`-' `-' `---'    `-' `-'`-'  `-' `-' `-'  `-'
+ * @author Bryan James Bassett, Kent State University c/o 2016
+ * -------------------"I've always dreamed of working at Kent"-----------------------*
  */
 public class BowlingGame {
     private int roll = 0;
@@ -15,7 +25,6 @@ public class BowlingGame {
     public int score(){
         int score = 0;
         int cursor = 0;
-        
         for (int frame = 0; frame < 10;frame++){
             if(isStrike(cursor)){ 
                 score += 10 + rolls[cursor+1] + rolls[cursor+2];
@@ -28,10 +37,7 @@ public class BowlingGame {
                 score += rolls[cursor] + rolls[cursor+1];
                 cursor += 2;       
             }
-            
-       
         }
-        
         return score;
     }
     private boolean isStrike(int cursor) {
